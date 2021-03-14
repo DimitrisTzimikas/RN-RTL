6f50c8dc5710e88ef6592e878d951f6fe8adb7b4
Dimitris Tzimikas <dimitris92a@hotmail.com>
2021-03-14 20:06:38 +0200

    Configure rtl

--- android/app/src/main/java/com/rtl/MainActivity.java
+++ android/app/src/main/java/com/rtl/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rtl;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -8,6 +10,14 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+    I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
+    sharedI18nUtilInstance.allowRTL(this, true);
+
+    super.onCreate(savedInstanceState);
+   }
+
   @Override
   protected String getMainComponentName() {
     return "rtl";
