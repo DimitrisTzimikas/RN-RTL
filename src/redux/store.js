0babcabe727156aab6edb5c2d1479cf4afcec7e3
Dimitris Tzimikas <dimitris92a@hotmail.com>
2021-03-14 22:52:43 +0200

    Add flipper debugger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 /* Libraries */
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {createContext} from 'react';
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -13,8 +13,19 @@ const config = {
   whitelist: ['language'],
   blacklist: [''],
 };
+
+const middlewares = [];
+
+if (__DEV__) {
+  const createDebugger = require('redux-flipper').default;
+  middlewares.push(createDebugger());
+}
+
 const persistedReducer = persistReducer(config, reducers);
-const store = createStore(persistedReducer, composeWithDevTools());
+const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(...middlewares)),
+);
 const persistor = persistStore(store);
 
 const LocalizationContext = createContext();
